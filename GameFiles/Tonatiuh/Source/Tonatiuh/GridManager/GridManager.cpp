e6d4f3f8bc285799f8d7fd1b2cb1f00cda85a3a7
wighthood <60099411+wighthood@users.noreply.github.com>
2025-05-28 12:01:35 +0200

    testing to some stuff

--- GameFiles/Tonatiuh/Source/Tonatiuh/GridManager/GridManager.cpp
+++ GameFiles/Tonatiuh/Source/Tonatiuh/GridManager/GridManager.cpp
@@ -9,7 +9,10 @@ AGridManager::AGridManager()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
 }
+
+
 
 // Called when the game starts or when spawned
 void AGridManager::BeginPlay()
