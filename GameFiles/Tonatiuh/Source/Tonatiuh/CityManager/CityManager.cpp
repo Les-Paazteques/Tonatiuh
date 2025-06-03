9051f7e7c4fe69d660d761a62e3a439d5b68d119
wighthood <60099411+wighthood@users.noreply.github.com>
2025-06-03 16:17:59 +0200

    created citymanager

---
+++ GameFiles/Tonatiuh/Source/Tonatiuh/CityManager/CityManager.cpp
@@ -0,0 +1,55 @@
+// Made by 'Les Paaztèques', check out game's credits for more information.
+
+
+#include "CityManager.h"
+
+
+// Sets default values
+ACityManager::ACityManager()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ACityManager::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ACityManager::produceResource()
+{
+	for (auto[Name,value]: resources)
+	{
+		if (resources[Name] >= resourcesCap[Name] ||
+			(resources[Name] <= 0 && resourcesGain[Name] < 0))
+			return;
+		if (resources[Name]+resourcesGain[Name] > resourcesCap[Name])
+		{
+			resources[Name] += resourcesCap[Name]-resourcesGain[Name];
+			return;
+		}
+		if (resources[Name]-resourcesGain[Name] < 0)
+		{
+			resources[Name] = 0;
+			return;
+		}
+		resources[Name] += resourcesGain[Name];
+	}
+}
+
+void ACityManager::UpdateResourcegain()
+{
+	
+	for (auto[Name,value]: resourcesGain)
+	{
+		//resourcesGain[Name] += 
+	}
+}
+
+// Called every frame
+void ACityManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ GameFiles/Tonatiuh/Source/Tonatiuh/CityManager/CityManager.h
@@ -0,0 +1,39 @@
+// Made by 'Les Paaztèques', check out game's credits for more information.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Tonatiuh/Enums/EJobEnum.h"
+#include "Tonatiuh/Enums/EResourceEnum.h"
+#include "CityManager.generated.h"
+
+UCLASS()
+class TONATIUH_API ACityManager : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ACityManager();
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void produceResource();
+
+	void UpdateResourcegain();
+
+	UPROPERTY(EditAnywhere)
+	TMap<EResourceEnum, int> resources;
+
+	UPROPERTY(EditAnywhere)
+	TMap<EResourceEnum, int> resourcesCap;
+
+	UPROPERTY(EditAnywhere)
+	TMap<EResourceEnum, int> resourcesGain;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
