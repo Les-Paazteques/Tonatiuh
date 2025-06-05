e5079963fc855b1a8c1dbe0a7ada6a4c652e65ba
Keuvain <118433891+Sarulpabi1@users.noreply.github.com>
2025-06-05 10:11:04 +0200

    Adding Player Health System

--- GameFiles/Tonatiuh/Source/Tonatiuh/HealthComponent/PlayerHealthComponent.cpp
+++ GameFiles/Tonatiuh/Source/Tonatiuh/HealthComponent/PlayerHealthComponent.cpp
@@ -3,11 +3,14 @@
 #include "PlayerHealthComponent.h"
 #include "Engine/World.h"
 #include "Tonatiuh/TonatiuhCharacter.h"
+#include "Tonatiuh/Character/MetroidVaniaCharacter.h"
 
 UPlayerHealthComponent::UPlayerHealthComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
 	RespawnLocation = FVector::ZeroVector;
+	MaxHealth = 5;
+	CurrentHealth = MaxHealth;
 }
 
 void UPlayerHealthComponent::BeginPlay()
@@ -33,15 +36,57 @@ FVector UPlayerHealthComponent::GetRespawnLocation() const
 
 void UPlayerHealthComponent::Die()
 {
+	CurrentHealth = MaxHealth;
+	OnDeath.Broadcast();
 	Respawn();
 }
 
 void UPlayerHealthComponent::Respawn()
 {
-	ATonatiuhCharacter* owner = Cast<ATonatiuhCharacter>(GetOwner());
+	AMetroidVaniaCharacter* owner = Cast<AMetroidVaniaCharacter>(GetOwner()->GetClass());
+	owner->SetActorLocation(RespawnLocation);
+    
+	OnRespawn.Broadcast();
+}
 
-	if (owner && !RespawnLocation.IsZero())
-	{
-		owner->SetActorLocation(RespawnLocation);
-	}
+void UPlayerHealthComponent::TakeDamage(int p_damageAmount)
+{
+	if (p_damageAmount <= 0)
+		return;
+
+	CurrentHealth -= p_damageAmount;
+	CurrentHealth = FMath::Clamp(CurrentHealth, 0, MaxHealth);
+	
+
+	OnDamaged.Broadcast(p_damageAmount);
+
+	if (CurrentHealth <= 0)
+		this->Die(); 
+}
+
+void UPlayerHealthComponent::HealDamage(int p_damageAmount)
+{
+	CurrentHealth += p_damageAmount;
+	CurrentHealth = FMath::Clamp(CurrentHealth, 0, MaxHealth);
+	
+	OnDamaged.Broadcast(p_damageAmount);
+}
+
+void UPlayerHealthComponent::IncreaseMaxHealth(int p_healthAmount)
+{
+	MaxHealth += p_healthAmount;
+	CurrentHealth += p_healthAmount;
+
+	OnMaxHealthChange.Broadcast(p_healthAmount);
+}
+
+void UPlayerHealthComponent::DecreaseMaxHealth(int p_healthAmount)
+{
+	if (p_healthAmount <= 0 || MaxHealth <= 0 || CurrentHealth <= 0)
+		return;
+	
+	MaxHealth -= p_healthAmount;
+	CurrentHealth -= p_healthAmount;
+
+	OnMaxHealthChange.Broadcast(p_healthAmount);
 }
